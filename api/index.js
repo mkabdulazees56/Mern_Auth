e65df57bfbe4f823f98b4fe5eb59bafc4a13d7a6
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-07-05 18:28:29 +0500

    connected to cloud mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("Connected to Mongo Db");
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
-app.listen(3000,()=>{
-    console.log('Sever listning on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Sever listning on port 3000");
+});
