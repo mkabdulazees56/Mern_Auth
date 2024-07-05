65380654dfcc27e0aa990b26127c21e474324073
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-07-05 16:50:51 +0500

    Navbar and footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import Home from './components/Home'
-import Signup from './components/Signup'
-import About from './components/About'
-import Signin from './components/Signin'
-import { Route, Routes } from 'react-router-dom'
-import Profile from './components/Profile'
+import React from "react";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import About from "./components/About";
+import Signin from "./components/Signin";
+import { Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-  <Routes>
-    <Route path="/" element ={<Home/>}> </Route>
-    <Route path="/About" element ={<About/>}> </Route>
-    <Route path="/signup" element ={<Signup/>}> </Route>
-    <Route path="/signin" element ={<Signin/>}> </Route>
-    <Route path="/profile" element ={<Profile/>}> </Route>
-
-
-  </Routes>
-  )
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {" "}
+        </Route>
+        <Route path="/About" element={<About />}>
+          {" "}
+        </Route>
+        <Route path="/signup" element={<Signup />}>
+          {" "}
+        </Route>
+        <Route path="/signin" element={<Signin />}>
+          {" "}
+        </Route>
+        <Route path="/profile" element={<Profile />}>
+          {" "}
+        </Route>
+      </Routes>
+      <Footer/>
+    </>
+  );
 }
 
-export default App
+export default App;
